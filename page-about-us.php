c4276f59fd44c33340cd52876b68661f9ee4d666
reasonsx <kamkre01@easv365.dk>
2025-05-12 01:06:21 +0200

    Update page-about-us.php

--- page-about-us.php
+++ page-about-us.php
@@ -39,8 +39,8 @@ get_header(); ?>
         <li class="mb-10 ms-4">
             <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white"></div>
             <time class="mb-1 text-sm font-normal leading-none text-gray-400">February 2022</time>
-            <h3 class="text-lg font-semibold text-gray-900">Application UI code in Tailwind CSS</h3>
-            <p class="mb-4 text-base font-normal text-gray-500">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+            <h3 class="text-lg font-semibold text-gray-900">Title</h3>
+            <p class="mb-4 text-base font-normal text-gray-500">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700">
                 Learn more
                 <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -51,12 +51,12 @@ get_header(); ?>
         <li class="mb-10 ms-4">
             <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white"></div>
             <time class="mb-1 text-sm font-normal leading-none text-gray-400">March 2022</time>
-            <h3 class="text-lg font-semibold text-gray-900">Marketing UI design in Figma</h3>
-            <p class="text-base font-normal text-gray-500">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+            <h3 class="text-lg font-semibold text-gray-900">Title</h3>
+            <p class="text-base font-normal text-gray-500">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
         </li>
         <li class="ms-4">
             <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white"></div>
-            <time class="mb-1 text-sm font-normal leading-none text-gray-400">May 29th, 1954</time>
+            <time class="mb-1 text-sm font-normal leading-none text-gray-400">May 29, 1954</time>
             <h3 class="text-lg font-semibold text-gray-900">Beginning of Sønderborg Archery Guild</h3>
             <p class="text-base font-normal text-gray-500">Sønderborg Archery Guild was founded on May 29, 1954 and is a member of Archery Denmark.</p>
         </li>
