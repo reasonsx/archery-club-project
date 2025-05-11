53388a5369d11148c8e9b975e4d47d46df06f85e
reasonsx <kamkre01@easv365.dk>
2025-05-11 17:56:55 +0200

    Update header.php

--- header.php
+++ header.php
@@ -78,7 +78,7 @@
                 <?php if (function_exists('pll_the_languages')) :
                     $langs_array = pll_the_languages([
                         'dropdown' => 1,
-                        'hide_current' => 0, // SHOW current language too
+                        'hide_current' => 0,
                         'raw' => 1
                     ]);
                     $current_lang = pll_current_language();
@@ -111,40 +111,7 @@
                             </ul>
                         </div>
                     </div>
-
-
                 <?php endif; ?>
-
-
-                <!--                --><?php //if (function_exists('pll_the_languages')) : ?>
-<!--                    <div class="language-switcher">-->
-<!--                        --><?php //pll_the_languages(array('dropdown' => 1)); ?>
-<!--                    </div>-->
-<!--                --><?php //endif; ?>
-<!---->
-<!---->
-<!--                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"-->
-<!--                        class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"-->
-<!--                        type="button">Dropdown button-->
-<!--                    <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"-->
-<!--                         viewBox="0 0 10 6">-->
-<!--                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                              d="m1 1 4 4 4-4"/>-->
-<!--                    </svg>-->
-<!--                </button>-->
-<!---->
-<!--                <!-- Dropdown menu -->-->
-<!--                <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44">-->
-<!--                    <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownDefaultButton">-->
-<!--                        <li>-->
-<!--                            <a href="#" class="block px-4 py-2 hover:bg-gray-100">Dashboard</a>-->
-<!--                        </li>-->
-<!--                        <li>-->
-<!--                            <a href="#" class="block px-4 py-2 hover:bg-gray-100">Settings</a>-->
-<!--                        </li>-->
-<!---->
-<!--                    </ul>-->
-<!--                </div>-->
 
 
             </div>
