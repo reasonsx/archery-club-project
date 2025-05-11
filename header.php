7647f2f834f2a6df70a0b5cc06879d78d18d558e
reasonsx <kamkre01@easv365.dk>
2025-05-12 00:22:28 +0200

    ok

--- header.php
+++ header.php
@@ -5,84 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
 <header>
-    <nav class="bg-white border-gray-200 rounded px-4 lg:px-6 py-2.5">
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <!-- Logo -->
-            <a href="<?php echo home_url(); ?>" class="flex items-center">
-                <img src="https://www.s-bl.dk/images/SBL_logo_2017_lille_sort_skrift.jpg" class="mr-3 h-6 sm:h-9"
-                     alt="Flowbite Logo"/>
-                <span class="self-center text-xl font-semibold whitespace-nowrap">Sønderborg Bueskyttelaug</span>
-            </a>
-            <div class="flex items-center lg:order-2">
-<!--                <a href="#"-->
-<!--                   class="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Log-->
-<!--                    in</a>-->
-<!--                <a href="#"-->
-<!--                   class="text-white bg-red-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Get-->
-<!--                    started</a>-->
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+        <div class="flex items-center justify-between mx-auto max-w-screen-xl">
 
-                <!-- Mobile menu button -->
-                <button data-collapse-toggle="mobile-menu-2" type="button"
-                        class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                        aria-controls="mobile-menu-2" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                              clip-rule="evenodd"></path>
-                    </svg>
-                    <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                              clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-
+            <!-- Left: Logo and Name -->
+            <div class="flex items-center flex-shrink-0">
+                <a href="<?php echo home_url(); ?>" class="flex items-center">
+                    <img src="https://www.s-bl.dk/images/SBL_logo_2017_lille_sort_skrift.jpg" class="mr-3 h-6 sm:h-9" alt="SBL Logo" />
+                    <span class="self-center text-xl font-semibold whitespace-nowrap">Sønderborg Bueskyttelaug</span>
+                </a>
             </div>
 
-
-            <div class="hidden justify-between  items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                    <li>
-                        <a href="<?php echo home_url(); ?>"
-                           class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Home</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo get_permalink(get_page_by_path('about-us')); ?>"
-                           class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">About
-                            us</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo get_permalink(get_option('page_for_posts')); ?>"
-                           class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Blog</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo get_permalink(get_page_by_path('how-to-join')); ?>"
-                           class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">How
-                            to join</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                           class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Contact</a>
-                    </li>
+            <!-- Center: Main Navigation (Desktop only) -->
+            <div class="hidden lg:flex justify-center flex-1">
+                <ul class="flex flex-row space-x-8 font-medium text-gray-700">
+                    <li><a href="<?php echo home_url(); ?>" class="hover:text-blue-700">Home</a></li>
+                    <li><a href="<?php echo get_permalink(get_page_by_path('about-us')); ?>" class="hover:text-blue-700">About us</a></li>
+                    <li><a href="<?php echo get_permalink(get_option('page_for_posts')); ?>" class="hover:text-blue-700">Blog</a></li>
+                    <li><a href="<?php echo get_permalink(get_page_by_path('how-to-join')); ?>" class="hover:text-blue-700">How to join</a></li>
+                    <li><a href="#" class="hover:text-blue-700">Contact</a></li>
                 </ul>
+            </div>
+
+            <!-- Right: Language Switcher -->
+            <div class="flex items-center space-x-4">
                 <?php
                 function get_custom_lang_flag_svg($lang_slug) {
-                    $flag_path = get_template_directory_uri() . '/assets/flags/' . $lang_slug . '.svg';
-                    return $flag_path;
+                    return get_template_directory_uri() . '/assets/flags/' . $lang_slug . '.svg';
                 }
-                ?>
 
-                <?php if (function_exists('pll_the_languages')) :
+                if (function_exists('pll_the_languages')) :
                     $langs_array = pll_the_languages([
                         'dropdown' => 1,
                         'hide_current' => 0,
@@ -93,11 +51,11 @@
                     <div class="relative inline-block text-left">
                         <!-- Trigger Button -->
                         <button id="langDropdownButton" data-dropdown-toggle="langDropdown"
-                                class="cursor-pointer text-white bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-sky-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center w-36"
+                                class="cursor-pointer text-white bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-sky-400 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
                                 type="button">
                             <img src="<?= esc_url(get_custom_lang_flag_svg($current_lang)); ?>" alt="" class="w-5 h-5 mr-2">
                             <?= esc_html($langs_array[$current_lang]['name']); ?>
-                            <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            <svg class="w-2.5 h-2.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                  viewBox="0 0 10 6">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                       d="m1 1 4 4 4-4"/>
@@ -122,8 +80,29 @@
                     </div>
                 <?php endif; ?>
 
+                <!-- Mobile menu toggle -->
+                <button data-collapse-toggle="mobile-menu-2" type="button"
+                        class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                        aria-controls="mobile-menu-2" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd"
+                              d="M3 5h14a1 1 0 010 2H3a1 1 0 010-2zm0 5h14a1 1 0 010 2H3a1 1 0 010-2zm0 5h14a1 1 0 010 2H3a1 1 0 010-2z"
+                              clip-rule="evenodd"></path>
+                    </svg>
+                </button>
             </div>
+        </div>
 
+        <!-- Mobile Navigation -->
+        <div class="hidden justify-between items-center w-full lg:hidden" id="mobile-menu-2">
+            <ul class="flex flex-col mt-4 font-medium text-gray-700 space-y-2">
+                <li><a href="<?php echo home_url(); ?>" class="block py-2 px-4 hover:bg-gray-100">Home</a></li>
+                <li><a href="<?php echo get_permalink(get_page_by_path('about-us')); ?>" class="block py-2 px-4 hover:bg-gray-100">About us</a></li>
+                <li><a href="<?php echo get_permalink(get_option('page_for_posts')); ?>" class="block py-2 px-4 hover:bg-gray-100">Blog</a></li>
+                <li><a href="<?php echo get_permalink(get_page_by_path('how-to-join')); ?>" class="block py-2 px-4 hover:bg-gray-100">How to join</a></li>
+                <li><a href="#" class="block py-2 px-4 hover:bg-gray-100">Contact</a></li>
+            </ul>
         </div>
     </nav>
 </header>
