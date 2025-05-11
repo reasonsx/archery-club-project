ad3c9756dc5c2df57ff01a1660f1b0df9008012a
reasonsx <kamkre01@easv365.dk>
2025-05-11 14:18:21 +0200

    Update header.php

--- header.php
+++ header.php
@@ -57,6 +57,10 @@ TEST IF IT WORKS
             <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
+                        <a href="<?php echo home_url(); ?>"
+                           class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Home</a>
+                    </li>
+                    <li>
                         <a href="<?php echo get_permalink( get_page_by_path( 'about-us' ) ); ?>"
                            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">About us</a>
                     </li>
