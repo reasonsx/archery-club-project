2d03b52c3de77292a0c638271ce6e5b92c82d552
reasonsx <kamkre01@easv365.dk>
2025-05-11 15:47:51 +0200

    Update header.php

--- header.php
+++ header.php
@@ -11,12 +11,12 @@
 <body <?php body_class(); ?>>
 
 <header>
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+    <nav class="bg-white border-gray-200 rounded px-4 lg:px-6 py-2.5">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <!-- Logo -->
             <a href="<?php echo home_url(); ?>" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
-                <span class="self-center text-xl font-semibold whitespace-nowrap">Flowbite</span>
+                <img src="https://www.s-bl.dk/images/SBL_logo_2017_lille_sort_skrift.jpg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
+                <span class="self-center text-xl font-semibold whitespace-nowrap">Sønderborg Bueskyttelaug</span>
             </a>
             <div class="flex items-center lg:order-2">
                 <a href="#"
@@ -47,7 +47,7 @@
             </div>
 
 
-            <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+            <div class="hidden justify-between  items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
                         <a href="<?php echo home_url(); ?>"
