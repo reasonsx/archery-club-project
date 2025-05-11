1c3cca5feaec7239defe14ff1eb7c341f0b96206
reasonsx <kamkre01@easv365.dk>
2025-05-11 14:13:11 +0200

    Update header.php

--- header.php
+++ header.php
@@ -58,8 +58,7 @@ TEST IF IT WORKS
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
                         <a href="#"
-                           class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0"
-                           aria-current="page">About us</a>
+                           class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">About us</a>
                     </li>
                     <li>
                         <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>"
