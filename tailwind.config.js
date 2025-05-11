4b06512c05b2e4c275bf6aad1145891f45bc0b1f
reasonsx <kamkre01@easv365.dk>
2025-05-11 16:38:28 +0200

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+                "sans": ["Inter", ...defaultTheme.fontFamily.sans]
             }
         }
     }
