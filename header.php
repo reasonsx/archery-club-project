b1d898112e486a180e6315ef678fbd501cf799a0
reasonsx <kamkre01@easv365.dk>
2025-05-11 13:05:40 +0200

    added blog

--- header.php
+++ header.php
@@ -63,7 +63,7 @@ TEST IF IT WORKS
                     </li>
                     <li>
                         <a href="#"
-                           class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Blog</a>
+                           class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"><?php echo get_permalink( get_option( 'page_for_posts' ) ); ?></a>
                     </li>
                     <li>
                         <a href="#"
---
+++ page-blog.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Blog Page
+ */
+
+get_header(); ?>
+
+<div class="content">
+    <h1>Blog</h1>
+
+    <?php
+    // WordPress loop to display blog posts
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            ?>
+            <div class="post">
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <p><?php the_excerpt(); ?></p>
+            </div>
+            <?php
+        endwhile;
+    else :
+        echo '<p>No posts found.</p>';
+    endif;
+    ?>
+</div>
+
+<?php get_footer(); ?>
