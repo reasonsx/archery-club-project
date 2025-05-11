a2ccdf43a28686424ad6bea7c0253d8403da853a
reasonsx <kamkre01@easv365.dk>
2025-05-11 12:15:00 +0200

    ok

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
     </div>
 </div>
 
-
+TEST IF IT WORKS
 <header>
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
