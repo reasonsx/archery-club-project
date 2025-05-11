d60d4643cd5296a83772ba08f98965fa83195ade
reasonsx <kamkre01@easv365.dk>
2025-05-11 16:03:52 +0200

    Update page-how-to-join.php

--- page-how-to-join.php
+++ page-how-to-join.php
@@ -6,7 +6,7 @@ Template Name: How to Join
 get_header(); ?>
 
 <main class="max-w-3xl mx-auto px-4 py-10">
-    <h1 class="text-3xl font-bold mb-6">How to Join TEXT FROM FILE CODE TEST</h1>
+    <h1 class="text-3xl font-bold mb-6"><?php the_title(); ?></h1>
     <div class="prose">
         <?php while ( have_posts() ) : the_post(); ?>
             <?php the_content(); ?>
