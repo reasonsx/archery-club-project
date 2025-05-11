83eb0b3f6e1e3fbcf14fa4a7e853282f3e401bba
reasonsx <kamkre01@easv365.dk>
2025-05-12 00:50:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ get_header(); ?>
     <h1 class="text-4xl">What is archery about</h1>
 </section>
 
-<section class="h-96 flex flex-col justify-center">
+<section class="flex flex-col justify-center">
     <h1 class="text-4xl">Learn more about archery</h1>
 
 
