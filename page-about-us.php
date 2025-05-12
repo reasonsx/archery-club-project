0e00c089a3735fef1b614008b876b41e29d95af7
reasonsx <kamkre01@easv365.dk>
2025-05-12 18:00:13 +0200

    Update page-about-us.php

--- page-about-us.php
+++ page-about-us.php
@@ -68,6 +68,7 @@ get_header(); ?>
             fun and exercise, but also those who want to train to be at
             an elite level in archery and, for example, become Danish Champion.
         </p>
+        <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Alternative</button>
     </div>
 </section>
 
