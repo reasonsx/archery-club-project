2c07e37ce54f6e5fc5df46be867b808f528306ff
reasonsx <kamkre01@easv365.dk>
2025-05-12 18:21:31 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,17 +1,15 @@
 <?php get_header(); ?>
 
 <main class="grid h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-  <div class="text-center">
-    <p class="text-base font-semibold text-indigo-600">404</p>
-    <h1>Page not found</h1>
-    <p class="mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
-    <div class="mt-10 flex items-center justify-center gap-x-6">
-        <a href="<?php echo home_url(); ?>" class="text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg px-5 py-2.5">
-            Go back home
-        </a>
-
+    <div class="text-center">
+        <p class="text-base font-semibold text-indigo-600">404</p>
+        <h1>Page not found</h1>
+        <p class="mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+               href="<?php echo home_url(); ?>">Go back home</a>
+        </div>
     </div>
-  </div>
 </main>
 
 <?php get_footer(); ?>
