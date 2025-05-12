b3eb61685e1edbfc6c27b0ce95d7d993866029bf
reasonsx <kamkre01@easv365.dk>
2025-05-12 18:24:44 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 
-<main class="grid h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-    <div class="text-center">
+<section class="grid h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 text-center">
         <p class="font-semibold text-blue-400">404</p>
         <h1>Page not found</h1>
         <p class="mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
@@ -9,7 +8,6 @@
             <a class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
                href="<?php echo home_url(); ?>">Go back home</a>
         </div>
-    </div>
-</main>
+</section>
 
 <?php get_footer(); ?>
