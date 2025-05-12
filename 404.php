40ee45b57919e96b0a9b1b65b13e25edc2200a42
reasonsx <kamkre01@easv365.dk>
2025-05-12 18:14:11 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
   <div class="text-center">
     <p class="text-base font-semibold text-indigo-600">404</p>
     <h1>Page not found</h1>
-    <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Sorry, we couldn’t find the page you’re looking for.</p>
+    <p class="mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
     <div class="mt-10 flex items-center justify-center gap-x-6">
       <a href="<?php echo home_url(); ?>" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
     </div>
