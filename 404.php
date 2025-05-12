7b5ad467de740911e0aeac008de995e14be45894
reasonsx <kamkre01@easv365.dk>
2025-05-12 18:18:15 +0200

    ok

--- 404.php
+++ 404.php
@@ -6,7 +6,12 @@
     <h1>Page not found</h1>
     <p class="mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
     <div class="mt-10 flex items-center justify-center gap-x-6">
-      <a href="<?php echo home_url(); ?>" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
+        <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+            <a href="<?php echo home_url(); ?>">Go back home</a>
+        </button>
+
+
+
     </div>
   </div>
 </main>
