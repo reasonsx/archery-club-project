30fd36ef91c0720d133b658d768ed8a9dd890d90
reasonsx <kamkre01@easv365.dk>
2025-05-14 18:37:31 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -56,13 +56,10 @@
                     <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Training Times</h2>
                     <ul class="text-gray-600">
                         <li class="mb-4">
-                            <span class="font-semibold">Monday:</span> 18:00 – 19:30
+                        Monday: 18:00 – 19:30
                         </li>
                         <li class="mb-4">
                             <span class="font-semibold">Thursday:</span> 18:00 – 19:30
-                        </li>
-                        <li class="mb-4">
-                            <a href="https://www.facebook.com/profile.php?id=100057323084944#" target="_blank" class="hover:underline">Facebook</a>
                         </li>
                     </ul>
                 </div>
