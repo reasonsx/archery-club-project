e1a970596b81887e8a19bef841f44e10d17832aa
nakaay13 <karbrz01@easv365.dk>
2025-05-14 18:49:26 +0200

    ok

--- page-how-to-join.php
+++ page-how-to-join.php
@@ -8,16 +8,18 @@ get_header(); ?>
 <?php $hero_title = get_field('join_hero_title'); ?>
 <?php $hero_description = get_field('join_hero_description'); ?>
 
-<section class="h-screen bg-no-repeat bg-center bg-cover flex items-center justify-center"
-    style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/svgs/pattern_big.svg');">
-  <div class="flex items-center justify-between w-full px-8">
+<section class="h-screen bg-no-repeat bg-center bg-cover flex items-center justify-center relative"
+    style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/svgs/pattern_big.svg'); z-index: -1;">
+  <div class="flex items-center justify-between w-full px-8 relative">
     
+    <!-- Right content (Image + Yellow Circle) -->
     <div class="w-1/2 flex flex-col items-center justify-center relative">
-      <img src="<?php echo $hero_image['sizes']['large']; ?>" alt="<?php echo $intro_image['alt']; ?>" class="w-48 h-48 object-contain mb-4" />
-      <!-- Yellow Circle -->
-      <div class="w-24 h-24 bg-yellow-400 rounded-full"></div>
+      <!-- Yellow Circle as Background -->
+      <div class="w-48 h-48 bg-[FDD576] rounded-full absolute z-0"></div>
+      <img src="<?php echo $hero_image['sizes']['large']; ?>" alt="<?php echo $intro_image['alt']; ?>" class="w-48 h-48 object-contain mb-4 relative z-10" />
     </div>
     
+    <!-- Left content (Text) -->
     <div class="w-1/2">
       <h1 class="text-4xl text-black font-bold mb-4"><?php 
         if ($hero_title) {
@@ -25,11 +27,11 @@ get_header(); ?>
         }
         ?>
       </h1>
-      <p class="text-lg"><?php 
+      <h3 class="text-lg"><?php 
         if ($hero_description) {
             echo nl2br(esc_html($hero_description));
         }
-      ?></p>
+      ?></h3>
     </div>
   </div>
 </section>
