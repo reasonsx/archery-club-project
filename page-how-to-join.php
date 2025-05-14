f1fd40232980bc8e5265fba426ef590bef3e9d09
nakaay13 <karbrz01@easv365.dk>
2025-05-14 18:35:17 +0200

    OK

--- page-how-to-join.php
+++ page-how-to-join.php
@@ -8,8 +8,8 @@ get_header(); ?>
 <?php $hero_title = get_field('join_hero_title'); ?>
 <?php $hero_description = get_field('join_hero_description'); ?>
 
-<section class="h-screen bg-[url('assets\svgs\pattern_big.svg')] bg-no-repeat bg-center bg-cover flex items-center justify-center">
-  <h1 class="text-4xl text-white font-bold"><?php 
+<section class="h-screen bg-[url('/assets/svgs/pattern_big.svg')] bg-no-repeat bg-center bg-cover flex items-center justify-center">
+  <h1 class="text-4xl text-black font-bold"><?php 
     if ($hero_title) {
         echo nl2br(esc_html($hero_title));
     }
