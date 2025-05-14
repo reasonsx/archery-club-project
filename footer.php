e6eb826c3017e571c421a07a52b6a330194fcb0f
reasonsx <kamkre01@easv365.dk>
2025-05-14 18:44:58 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -67,10 +67,10 @@
                     <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Training Places</h3>
                     <ul class="text-gray-600">
                         <li class="mb-4">
-                            <span>Winter:</span><br><span>Indoor training at Humlehøjhallen<br>(Stråbjergvej 1, 6400 Sønderborg)</span>
+                            <span>Summer:</span><br><span>Outdoor range at Ingolf Nielsens Vej,<br>6400 Sønderborg</span>
                         </li>
                         <li class="mb-4">
-                            <span>Summer:</span><br><span>Outdoor range at Ingolf Nielsens Vej,<br>6400 Sønderborg</span>
+                            <span>Winter:</span><br><span>Indoor training at Humlehøjhallen<br>(Stråbjergvej 1, 6400 Sønderborg)</span>
                         </li>
                     </ul>
                 </div>
