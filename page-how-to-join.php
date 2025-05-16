ee9bb4681cae365c9a69371a0ff05862ed0878a2
nakaay13 <karbrz01@easv365.dk>
2025-05-16 18:09:40 +0200

    Update page-how-to-join.php

--- page-how-to-join.php
+++ page-how-to-join.php
@@ -47,10 +47,16 @@ get_header(); ?>
     <div class="flex flex-row gap-5">
         <div class="flex flex-col w-auto mx-auto w-1/2 text-black">
             <p><?php if ($description_top) echo wp_kses_post(nl2br($description_top)); ?></p>
-            <p><?php if ($next_event_text) echo wp_kses_post(nl2br($next_event_text)); ?></p>
-            <p><?php if ($next_event_date) echo wp_kses_post(nl2br($next_event_date)); ?></p>
-            <p><?php if ($following_event_text) echo wp_kses_post(nl2br($following_event_text)); ?></p>
-            <p><?php if ($following_event_date) echo wp_kses_post(nl2br($following_event_date)); ?></p>
+            <div class="flex flex-row">
+              <p><?php if ($next_event_text) echo wp_kses_post(nl2br($next_event_text)); ?></p>
+                <p class="font-bold bg-[#FDD576] rounded px-2 py-1">
+                <?php if ($next_event_date) echo wp_kses_post(nl2br($next_event_date)); ?>
+              </p>
+            </div>
+            <div class="flex flex-row">
+              <p><?php if ($following_event_text) echo wp_kses_post(nl2br($following_event_text)); ?></p>
+              <p class="font-bold"><?php if ($following_event_date) echo wp_kses_post(nl2br($following_event_date)); ?></p>
+            </div>
             <p><?php if ($ready_to_join) echo wp_kses_post(nl2br($ready_to_join)); ?></p>
         </div>
         <div class="flex flex-col items-center w-[70%] mx-auto">
