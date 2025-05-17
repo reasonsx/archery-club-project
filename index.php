004921ab51d637f9db021921c068dd2ee319729c
reasonsx <kamkre01@easv365.dk>
2025-05-17 17:42:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -53,8 +53,7 @@ $about_description = get_field('about_description');
             <div class="max-w-xl">
                 <h1 class="text-4xl font-bold mb-4"><?php echo esc_html($about_title); ?></h1>
                 <p class="mb-6 text-gray-700">
-                <p class="text-lg text-white"><?php echo wp_kses_post(nl2br($about_description)); ?></p>
-                </p>
+                <p class="text-lg text-black"><?php echo wp_kses_post(nl2br($about_description)); ?></p>
                 <a href="#join" class="inline-block cursor-pointer focus:outline-none text-black bg-[#FDD576] hover:bg-[#fce169] focus:ring-4 focus:ring-[#fdd576]/50 font-medium rounded-lg text-base px-6 py-2 transition">
                     Join Now
                 </a>
