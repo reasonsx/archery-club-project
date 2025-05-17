89c1b6149b81c1903620dcbd50975f3ae759bcab
nakaay13 <karbrz01@easv365.dk>
2025-05-17 18:28:37 +0200

    ok

--- page-how-to-join.php
+++ page-how-to-join.php
@@ -116,9 +116,9 @@ get_header(); ?>
         $is_even = $index % 2 === 0;
         $row_class = $is_even ? 'bg-[#FDD576]' : '';
       ?>
-        <div class="flex justify-between items-center px-4 py-3 border-b border-black <?php echo $row_class; ?>">
+        <div class="flex justify-between items-center pt-4 py-3 border-b border-black <?php echo $row_class; ?>">
           <div class="text-xl">
-            <span class="uppercasen text-3xl"><?php the_title(); ?></span>
+            <span class="uppercase text-3xl"><?php the_title(); ?></span>
             <span class="text-sm text-gray-700 ml-2"><?php echo esc_html($age_range); ?></span>
           </div>
           <div class="text-right text-md font-semibold">
