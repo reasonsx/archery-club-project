e9d9648efe82bf53d72d95a23447c16c3777e51c
reasonsx <kamkre01@easv365.dk>
2025-05-17 18:01:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -28,9 +28,9 @@ $about_description = get_field('about_description');
                     <div class="max-w-2xl space-y-6 text-white">
                         <h1 class="text-4xl font-bold drop-shadow-lg"><?php echo esc_html($hero_title); ?></h1>
                         <p class="text-lg text-white"><?php echo wp_kses_post(nl2br($hero_description)); ?></p>
-                        <a href="<?php echo esc_url(get_permalink(get_page_by_path('how-to-join'))); ?>"
+                        <a href="<?php echo esc_url(get_permalink(get_page_by_path('about-us'))); ?>"
                            class="cursor-pointer focus:outline-none text-black bg-[#FDD576] hover:bg-[#fce169] focus:ring-4 focus:ring-[#fdd576]/50 font-medium rounded-lg text-base px-6 py-2 inline-block transition text-center">
-                            Join Now
+                            Read more
                         </a>
                     </div>
                 </div>
