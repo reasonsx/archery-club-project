7fd47fd4a55f23d6d6c87b6a305a0d38cb9f6d7b
reasonsx <kamkre01@easv365.dk>
2025-05-17 18:41:34 +0200

    dd
    d

--- index.php
+++ index.php
@@ -152,7 +152,7 @@ $about_description = get_field('about_description');
 
 
         <!-- BOWS SECTION -->
-        <section class="w-full h-auto bg-[#8DB7E1]">
+        <section class="w-full h-auto bg-[#8DB7E1] py-16">
             <section class="max-w-screen-xl mx-auto p-4 sm:p-6 lg:p-0 gap-16 flex flex-col ">
                 <div class="mx-auto max-w-screen-sm text-center">
                     <h1 class="text-gray-900 text-3xl font-semibold mb-6">What types of bows we use?</h1>
@@ -203,7 +203,7 @@ $about_description = get_field('about_description');
                 </div>
             </section>
         </section>
-        
+
 
         <section class="max-w-screen-lg mx-auto p-4 sm:p-6 lg:p-0 gap-16 flex flex-col">
             <div class="mx-auto max-w-screen-sm text-center">
