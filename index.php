addc8b27e7dd0850dc1d72a3c99f243eaa9f11c9
reasonsx <kamkre01@easv365.dk>
2025-05-17 14:28:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ get_header(); ?>
 
 <section class="flex flex-col gap-24">
 
-    <section class="h-full h-96 w-full bg-cover bg-center flex items-center justify-center" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/home-hero.jpg');">
+    <section class="h-96 w-full bg-cover bg-center flex items-center justify-center" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/home-hero.jpg');">
         <h1 class="text-4xl text-white drop-shadow-lg">Hero</h1>
     </section>
 
