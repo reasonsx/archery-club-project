d4aee4c9f4884c7b94f2357f5204b1599a1774e6
nakaay13 <karbrz01@easv365.dk>
2025-05-17 19:06:59 +0200

    Update page-blog.php

--- page-blog.php
+++ page-blog.php
@@ -4,6 +4,26 @@ Template Name: Blog Overview
 */
 get_header();
 ?>
+<section class="bg-no-repeat bg-center bg-cover relative"
+    style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/svgs/pattern_big.svg');">
+  
+  <div class="flex flex-col-reverse md:flex-row items-center md:justify-between w-full h-full px-8 relative">
+
+
+    <!-- Image on Right with Yellow Circle Background -->
+    <div class="md:w-1/2 md:h-1/2 relative flex items-end justify-center h-full">
+      <!-- Yellow Circle -->
+      <div class="w-96 h-96 bg-[#FDD576] rounded-full absolute top-1/2 md:top-auto md:bottom-0 transform -translate-y-1/2 md:translate-y-0 z-0"></div>
+
+      <!-- Image -->
+      <div class="w-full md:w-1/2 text-center md:text-left">
+      <h1 class="lg:text-7xl text-black font-bold mb-4">
+        <?php if ($hero_title) echo nl2br(esc_html($hero_title)); ?>
+      </h1>
+    </div>
+    </div>
+  </div>
+</section>
 
 <section class="max-w-screen-lg mx-auto pt-24 px-4">
   <h1 class="text-3xl font-bold mb-8 text-center">Blog</h1>
