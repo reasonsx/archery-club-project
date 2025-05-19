d031be882a558ef9ab5c3c5799db0667147e3ba5
nakaay13 <karbrz01@easv365.dk>
2025-05-19 18:27:17 +0200

    ok

--- page-about-us.php
+++ page-about-us.php
@@ -145,7 +145,7 @@ $story_button_link   = get_field('our_story_button_link');
               <?php if ( $img_url ) : ?>
                 <img src="<?php echo esc_url( $img_url ); ?>"
                      alt="<?php the_title_attribute(); ?>"
-                     class="w-40 h-40 object-contain" />
+                     class="w-46 h-46 object-contain" />
               <?php endif; ?>
             </div>
 
