a0bde94940003d216200e83744a710bc0c6dbfbf
reasonsx <kamkre01@easv365.dk>
2025-05-20 20:48:27 +0200

    Update single.php

--- single.php
+++ single.php
@@ -26,7 +26,7 @@
 
     <!-- Optional: Back to Blog link -->
     <div class="mt-12">
-      <a href="<?php echo get_permalink(get_page_by_path('blog')); ?>" class="text-blue-500 hover:underline">
+      <a href="<?php echo get_permalink(get_page_by_path('blog')); ?>" class="cursor-pointer focus:outline-none text-black bg-[#FDD576] focus:ring-4 focus:ring-[#fdd576]/50 font-medium rounded-lg text-base px-6 py-2 inline-block transition text-center">
         ← Back to Blog
       </a>
     </div>
