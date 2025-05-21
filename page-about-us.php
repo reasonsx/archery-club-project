03a62d73ecc3ce9542ca58e38c019f67470db365
reasonsx <kamkre01@easv365.dk>
2025-05-21 20:36:36 +0200

    Update page-about-us.php

--- page-about-us.php
+++ page-about-us.php
@@ -90,17 +90,8 @@ $photo_gallery_title = get_field('photo_gallery_title');
                             <?php echo esc_html($timeline_title); ?>
                         </h3>
                         <p class="mb-4 text-base font-normal text-gray-500">
-                            <?php echo esc_html($timeline_description); ?>
+                            <?php echo wp_kses_post(nl2br($timeline_description)); ?>
                         </p>
-                        <a href="<?php the_permalink(); ?>"
-                           class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700">
-                            Learn more
-                            <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                            </svg>
-                        </a>
                     </li>
                 <?php
                 endwhile;
