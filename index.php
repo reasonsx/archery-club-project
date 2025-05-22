b95e1644200f454b627d3200afdbcbfee32f1c87
reasonsx <kamkre01@easv365.dk>
2025-05-22 20:51:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -30,10 +30,13 @@ $benefits_title = get_field('benefits_title');
                     <div class="p-4 flex flex-col sm:p-0 md:w-1/2 w-full space-y-6 text-white">
                         <h1 class="text-4xl font-bold drop-shadow-lg"><?php echo esc_html($hero_title); ?></h1>
                         <p class="text-lg text-white"><?php echo wp_kses_post(nl2br($hero_description)); ?></p>
-                        <a href="<?php echo esc_url(get_permalink(get_page_by_path('how-to-join'))); ?>"
-                           class=" text-center cursor-pointer focus:outline-none text-black bg-[#FDD576] focus:ring-4 focus:ring-[#fdd576]/50 font-medium rounded-lg text-base px-6 py-2 transition">
-                            Join Now
-                        </a>
+                        <div class="w-32">
+                            <a href="<?php echo esc_url(get_permalink(get_page_by_path('how-to-join'))); ?>"
+                               class=" text-center cursor-pointer focus:outline-none text-black bg-[#FDD576] focus:ring-4 focus:ring-[#fdd576]/50 font-medium rounded-lg text-base px-6 py-2 transition">
+                                Join Now
+                            </a>
+                        </div>
+
                     </div>
                 </div>
             </div>
