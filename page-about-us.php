926c48c3191409e936b5c8700d4fb7c093523817
reasonsx <kamkre01@easv365.dk>
2025-05-23 17:15:49 +0200

    Update page-about-us.php

--- page-about-us.php
+++ page-about-us.php
@@ -60,7 +60,7 @@ $timeline_image = get_field('timeline_image');
 
     <!-- TIMELINE -->
     <section class="max-w-screen-xl mx-auto p-4 md:p-0 gap-8 flex flex-col">
-        <div class="bg-[#8DB7E1] overflow-hidden md:flex h-full min-h-[500px]">
+        <div class="bg-[#8DB7E1] overflow-hidden md:flex h-full min-h-[500px] rounded-xl">
             <!-- Left Image -->
             <div class="md:w-1/2 h-[400px] md:h-auto">
                 <img src="<?php echo esc_url($timeline_image['url']); ?>"
