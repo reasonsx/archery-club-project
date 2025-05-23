5eee1891abad03fd3a91e1956bb571fdb4db3f29
reasonsx <kamkre01@easv365.dk>
2025-05-23 17:20:48 +0200

    Update page-about-us.php

--- page-about-us.php
+++ page-about-us.php
@@ -60,6 +60,9 @@ $timeline_image = get_field('timeline_image');
 
     <!-- TIMELINE -->
     <section class="max-w-screen-xl mx-auto p-4 md:p-0 gap-8 flex flex-col">
+        <div class="mx-auto max-w-screen-sm text-center">
+            <h1 class="mb-6">What types of bows we use?</h1>
+        </div>
         <div class="bg-[#8DB7E1] overflow-hidden md:flex h-full min-h-[500px] rounded-xl">
             <!-- Left Image -->
             <div class="md:w-1/2 h-[400px] md:h-auto">
