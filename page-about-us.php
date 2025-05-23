0fd6786a055e573f42f89856f33dd71b123ed9a5
reasonsx <kamkre01@easv365.dk>
2025-05-23 14:19:27 +0200

    d

--- page-about-us.php
+++ page-about-us.php
@@ -32,11 +32,11 @@ $timeline_image = get_field('timeline_image');
         <div class="flex flex-row gap-10 items-start">
 
             <!-- BIG YEAR BAR -->
-            <div class="relative md:w-[20vw]">
+            <div class="relative md:w-[20vw] hidden md:block">
                 <div class="bg-[#8DB7E1] w-full h-full min-h-[500px]"></div>
                 <span class="absolute inset-0 flex items-center justify-center rotate-270">
         <h1 class="text-[120px] md:text-[140px] font-extrabold leading-none"><?php echo esc_html($year); ?></h1>
-      </span>
+    </span>
             </div>
 
             <!-- STORY CONTENT -->
