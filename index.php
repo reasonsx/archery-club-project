dcc69b96521f4ab42aede1c29275ea55f475c820
nakaay13 <karbrz01@easv365.dk>
2025-05-23 19:43:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -239,7 +239,7 @@ $phone_and_email = get_field('phone_and_email');
             <section class="h-auto grid grid-cols-1 md:grid-cols-2 items-center gap-16">
                 <!-- Left column -->
                 <div class="flex flex-col relative z-10">
-                    <h3><?php echo esc_html($contact_us_title); ?></h3>
+                    <!-- <h3><?php echo esc_html($contact_us_title); ?></h3> -->
                     <?php echo do_shortcode('[contact-form-7 id="f89891b" title="Contact form 1"]'); ?>
                 </div>
 
