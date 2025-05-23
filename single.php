31c85fdce829801c6989750c4cb7e7835ed3b341
reasonsx <kamkre01@easv365.dk>
2025-05-23 12:43:14 +0200

    Update single.php

--- single.php
+++ single.php
@@ -19,14 +19,14 @@
       <?php endif; ?>
 
       <!-- Content -->
-      <div class="content text-gray-800">
+      <div class="content text-gray-900">
         <?php the_content(); ?>
       </div>
     </article>
 
     <!-- Optional: Back to Blog link -->
     <div class="mt-12">
-      <a href="<?php echo get_permalink(get_page_by_path('blog')); ?>"  class="w-full md:w-auto block text-center cursor-pointer focus:outline-none text-black bg-[#FDD576] focus:ring-4 focus:ring-[#fdd576]/50 font-medium rounded-lg text-base px-6 py-2 transition">
+      <a href="<?php echo get_permalink(get_page_by_path('blog')); ?>"  class="w-full md:w-auto block text-center cursor-pointer focus:outline-none text-gray-900 bg-[#FDD576] focus:ring-4 focus:ring-[#fdd576]/50 font-medium rounded-lg text-base px-6 py-2 transition">
         <?php echo pll__('← Back to Blog'); ?>
       </a>
     </div>
