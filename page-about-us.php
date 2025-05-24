337439edd292080802fbae101ae858d29f89cd77
nakaay13 <karbrz01@easv365.dk>
2025-05-24 13:55:52 +0200

    Update page-about-us.php

--- page-about-us.php
+++ page-about-us.php
@@ -33,11 +33,7 @@ $bows_title = get_field('bows_title');
     <section class="max-w-screen-xl mx-auto p-4 sm:p-6 xl:p-0 gap-8 flex flex-col h-auto">
         <div class="flex flex-row gap-8 items-start">
 
-            <div class="w-full block md:hidden">
-                <div class="bg-[#8DB7E1] w-full h-full min-h-[500px]"></div>
-                <span class="flex items-center justify-center"><h1
-                            class="text-[120px] md:text-[140px] font-extrabold leading-none"><?php echo esc_html($year); ?></h1></span>
-            </div>
+            
 
             <!-- BIG YEAR BAR -->
             <div class="relative md:w-[40vw] lg:w-[30vw] hidden md:block">
@@ -51,7 +47,11 @@ $bows_title = get_field('bows_title');
                 <h1>
                     <?php echo esc_html($story_title); ?>
                 </h1>
-
+                <div class="w-full block md:hidden py-6">
+                <div class="bg-[#8DB7E1] w-full h-full min-h-[500px]"></div>
+                <span class="flex items-center justify-center"><h1
+                            class="text-[120px] md:text-[140px] font-extrabold leading-none"><?php echo esc_html($year); ?></h1></span>
+            </div>
                 <div class="prose max-w-none">
                     <?php if ($story_text) echo nl2br(esc_html($story_text)); ?>
                 </div>
